2098dd46db46850a041fba1223fdc49982f769db
Jeonha Kang <kangjeonha@gmail.com>
2024-07-16 22:14:23 +0900

    second-commit

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard'
 
 const ProductAll = () => {
+    const [productList, setProductList] = useState([])
+    const getProducts = async () => {
+        let url = 'http://localhost:5000/products'
+        let response = await fetch(url)
+        let data = await response.json()
+        setProductList(data)
+    }
+    useEffect(() => {
+        getProducts()
+    }, [])
     return (
         <div>
-            전체상품페이지
+            <ProductCard />
         </div>
     )
 }
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductDetail = () => {
     return (
         <div>
-            상품 상세 페이지
+            
         </div>
     )
 }
